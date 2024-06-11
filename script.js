35c28f7b60f479a48801556f3b261eb0ad10dade
Wonder <111242876+AmazedWonder@users.noreply.github.com>
2024-06-11 11:43:54 +0100

    Update script.js
    update function addTask

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const inputBox = document.getElementById("input-box")
-const listContainer = document.getElementById("list-container")
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
-document.getElementById("add-btn").addEventListener("click", addTask)
+document.getElementById("add-btn").addEventListener("click", addTask);
 
 function addTask(params) {
     if (inputBox.value == ""){
-        alert("Please input Task")
+        alert("Please input Task");
     }else{
-        let li = document.createElement("li")
-        li.innerHTML = inputBox.value
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = '<i class="fa-solid fa-trash"></i>';
+        li.appendChild(span);
+        listContainer.appendChild(li);
     }
 }
